66fa9c44e799fa54ef301bf8f4b304741749ad49
David O'Brien <david@xirus.com.au>
2020-09-12 14:55:50 +1000

    accept parameters

--- checkov/__init__.py
+++ checkov/__init__.py
@@ -3,13 +3,32 @@ import logging
 import azure.functions as func
 import os
 import subprocess
-
+import json
+import yaml
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    output = subprocess.run(["checkov", "-f", "./checkov/template.json", "--framework", "arm", "-o", "json"], capture_output=True)
+    provider = req.params.get('provider')
+    file_type = req.params.get('file_type')
+    req_body = req.get_json()
+
+    if file_type == 'json':
+        template = 'template.json'
+        with open(template, 'w') as outfile:
+            json.dump(req_body, outfile, indent=4)
+    elif file_type == 'yaml':
+        template = 'template.yaml'
+        with open(template, 'w') as outfile:
+            yaml.dump(req_body, outfile, indent=4)
+
+    output = subprocess.run(["checkov", "-f", template, "--framework", provider, "-o", "json", "--quiet"], capture_output=True)
+
+    if os.path.exists(template):
+        os.remove(template)
+    else:
+        print("The file does not exist")
     if output:
-        return func.HttpResponse(f"{output}")
+        return func.HttpResponse(f"{output.stdout}")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
