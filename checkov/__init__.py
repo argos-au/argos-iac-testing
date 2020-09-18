e5f7e1bf0bf1d0b6cab5f9c2e33a1e756a51ed96
David O'Brien <david@xirus.com.au>
2020-09-19 07:26:50 +1000

    add error handling

--- checkov/__init__.py
+++ checkov/__init__.py
@@ -8,9 +8,19 @@ import yaml
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     provider = req.params.get('provider')
+    if ((provider != "arm") and (provider != 'cloudformation')):
+        return func.HttpResponse(
+            "ERROR: provider = must be arm or cloudformation",
+            status_code=400
+        )
     file_type = req.params.get('file_type')
+    if ((file_type != "json") and (file_type != "yaml")):
+        return func.HttpResponse(
+            "ERROR: file_type = must be json or yaml",
+            status_code=400
+        )
     req_body = req.get_json()
-
+    template = ""
     if file_type == 'json':
         template = '/tmp/template.json'
         with open(template, 'w') as outfile:
